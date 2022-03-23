1a36ab7bf3eb9bb12b4cb3031176961ac2907170
Admin <Admin@DESKTOP-7VJ04NJ>
2022-03-24 01:50:24 +0300

    Add project files.

---
+++ Event1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var some_event = new Event();
+
+            some_event.OnKeyPressed += (Sender, Key) => Console.WriteLine(" You entered: " + Key);
+
+            some_event.Run();
+        }
+    }
+
+    public class Event
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            while (true)
+            {
+                char key = Console.ReadKey().KeyChar;
+
+                if (key == 'c') return;
+
+                OnKeyPressed?.Invoke(this, key);
+            }
+        }
+    }
+}
